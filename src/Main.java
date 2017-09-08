908631a2906261ff379531b311eb83878ee90722
rohithgunda <31741092+rohithgunda@users.noreply.github.com>
2017-09-08 16:09:02 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,15 @@ public class Main {
 		//You may test that your code works find here
 		//Please check that your code works and has no 
 		//compilation problems before to submit
-	}
+		Scanner sc = new Scanner(System.in);
+    private int id;
+	private String fullname;
+	private Date birthDate;
+	private double avgMark;
+		Date stdob;
 
-}
+		System.out.print("How many students would you like to enter?");
+		n = sc.nextInt();
+		sc.nextLine();
+		ArrayList student = new ArrayList(n);
+	}
